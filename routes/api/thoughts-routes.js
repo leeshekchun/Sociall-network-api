404a666a6d127832ac6461f39f967231cf8de5c8
ShekChun Lee <leeshekchun@gmail.com>
2022-08-21 11:57:54 -0600

    create thought routes and make connection between index.js

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
